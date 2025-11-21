6e4052c2df9de867ae55965059f6f16169630134
JuanEstebanHG12 <juanestebanhg12@gmail.com>
2025-11-21 13:49:23 -0500

    Estadisticas unidades Totales y valor Total

--- user_historyS3M1/servicios.py
+++ user_historyS3M1/servicios.py
@@ -25,6 +25,9 @@ def validate_entrys(tipo, prompt, validacion_extra = None):
 def create_product():
     product = {}
     
+    def validar_len(x):
+        if len(x) > 0:
+            return True
     #solicitar 
     name_prod = validate_entrys(str, "Ingresar nombre del producto: ", lambda x:len(x) > 0)
     
@@ -47,20 +50,54 @@ def show_inventory(products_list):
 
 
 def calculate_stats():
+    print("1. Unidades Totales")
+    print("2. Valor Total")
+    print("3. Producto más caro")
+    print("4. Producto Mayor Stock")
+    opcion = input("Ingresar una opcion: ")
     products_list = load_csv()
+
+    if opcion == '1':
+        print(unidades_totales(products_list))
+    elif opcion == '2':
+        valor_total(products_list)
+    
+        
+    # sum_total = 0
+    # print("--------------------------------------------------------------------------------------------")
+    # print(f"{'Producto':<15}|{'Precio':<10}|{'cantidad':<10}|{'Total':<10}")
+    # for product in products_list:
+    #     total = float(product["price"])*int(product["amount"])
+    #     sum_total += total
+    #     sum_products += int(product["amount"])
+    #     print("--------------------------------------------------------------------------------------------")
+    #     print(f"{product["name"]:<15}|{product["price"]:<10}|{product["amount"]:<10}|{total:<10}")
+    #     print("--------------------------------------------------------------------------------------------")
+      
+    # print(f"{'Totales':<15}|{'':<10}|{sum_products:<10}|{sum_total:<10}\n")
+    # print(total)
+
+def unidades_totales(products_list):
+    sum_products = 0
+    print(f"{'Producto':<15}|{'cantidad':<10}")
+    for product in products_list:
+        print(f"{product['name']:<15}|{product['amount']:10}")
+        sum_products += int(product['amount'])
+    return f"{'Total':<15}|{sum_products:<10}\n"
+
+def valor_total(products_list):
     total = 0
     sum_total = 0
-    sum_products = 0
     print("--------------------------------------------------------------------------------------------")
     print(f"{'Producto':<15}|{'Precio':<10}|{'cantidad':<10}|{'Total':<10}")
     for product in products_list:
         total = float(product["price"])*int(product["amount"])
         sum_total += total
-        sum_products += int(product["amount"])
         print("--------------------------------------------------------------------------------------------")
         print(f"{product["name"]:<15}|{product["price"]:<10}|{product["amount"]:<10}|{total:<10}")
         print("--------------------------------------------------------------------------------------------")
-      
-    print(f"{'Totales':<15}|{'':<10}|{sum_products:<10}|{sum_total:<10}\n")
-    print(total)
+    return f"{'Total':<15}|{'':<10}|{'':<10}|{'':<10}|{sum_total:<10}\n"
+
+    
+
 
